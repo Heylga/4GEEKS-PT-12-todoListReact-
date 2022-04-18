432450e9f674688ce716ca15fbafb88aaee8652b
Heylga <oy.golovina@gmail.com>
2022-04-18 10:48:21 +0000

    position relative block

--- src/js/component/Todo.jsx
+++ src/js/component/Todo.jsx
@@ -6,6 +6,7 @@ const Todo = ({ text, todo, todos, setTodos }) => {
 	const deleteHandler = () => {
 		setTodos(todos.filter((item) => item.id !== todo.id));
 	};
+
 	return (
 		<div className="todo border">
 			<li className="todo-item list-group-item d-flex justify-content-center">
